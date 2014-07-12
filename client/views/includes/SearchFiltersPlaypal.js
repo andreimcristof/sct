3a07151da34265dadaa3fc4053b8508c104059bc
Andrei Cristof <andreimcristof@gmail.com>
2014-07-12 15:38:50 -0700

    filtering hint, enabled mainpage

--- client/views/includes/SearchFiltersPlaypal.js
+++ client/views/includes/SearchFiltersPlaypal.js
@@ -1,5 +1,4 @@
 SearchFiltersPlaypal = new Meteor.Collection(null);
-//SearchFiltersPlaypal.insert({"objectType": "test1", "name": "test1"});
 
 insertSearchFilterPlaypal = function(filter) {
   SearchFiltersPlaypal.insert(
@@ -15,5 +14,76 @@ removeSearchFilterPlaypal = function(filter) {
 Template.searchFiltersPlaypal.helpers({  
 	searchFiltersPlaypal: function() {  
 		return SearchFiltersPlaypal.find();
-	} 
+	},
+	isEmpty: function() {  
+		return SearchFiltersPlaypal.find().count() == 0;
+	}
+	
+});
+
+Template.searchFiltersPlaypal.playpalRaceFilter = function(){
+	
+		var racesChosen = SearchFiltersPlaypal.find({objectType : 'race'}).fetch();
+		if(racesChosen.length == 0)
+			return "all races";
+		else
+		{
+			var races = [];
+			for(var i = 0; i < racesChosen.length; i++)
+			{
+				var name = racesChosen[i].name;
+				races.push(name);
+			}
+			return races.join(", ");
+		}
+}
+
+
+Template.searchFiltersPlaypal.playpalServerFilter = function(){
+	
+		var serversChosen = SearchFiltersPlaypal.find({objectType : 'server'}).fetch();
+		if(serversChosen.length == 0)
+			return "all servers";
+		else
+		{
+			var servers = [];
+			for(var i = 0; i < serversChosen.length; i++)
+			{
+				var name = serversChosen[i].name;
+				servers.push(name);
+			}
+			return servers.join(", ");
+		}
+}
+
+Template.searchFiltersPlaypal.playpalLeagueFilter = function(){
+	
+		var leaguesChosen = SearchFiltersPlaypal.find({objectType : 'league'}).fetch();
+		if(leaguesChosen.length == 0)
+			return "all leagues";
+		else
+		{
+			var leagues = [];
+			for(var i = 0; i < leaguesChosen.length; i++)
+			{
+				var name = leaguesChosen[i].name;
+				leagues.push(name);
+			}
+			return leagues.join(", ");
+		}
+}
+
+
+Template.searchFiltersPlaypal.rendered = function(){
+	SearchFiltersPlaypal.remove({});
+}
+
+
+Template.searchFiltersPlaypal.events({
+	'click #removeFilters': function(e){
+		e.preventDefault();
+
+		SearchFiltersPlaypal.remove({});
+		$('.checkboxListItem').prop('checked', false);
+	}
 });
--- utils.js
+++ utils.js
@@ -2,3 +2,5 @@ Handlebars.registerHelper("readableDate", function(timestamp) {
     return new Date(timestamp).toDateString()
 });
 
+
+//console.log(Users.find({$or: [{email: 'some@mail.com'},{city: 'atlanta'}]}).fetch());
