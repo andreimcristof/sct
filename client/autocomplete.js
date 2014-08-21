e102df05cf45f4668fc316c25ffcf4bdfbc4bc21
Andrei Cristof <andreimcristof@gmail.com>
2014-08-22 00:31:40 +0200

    fixed colors on searchbar

--- client/autocomplete.js
+++ client/autocomplete.js
@@ -6,24 +6,24 @@ Template.autoComplete.settings = function() {
    position: "bottom",
    limit: 10,
    rules: [
-     {
-       token: '',
-       collection: Strategies,
-       field: "name",
-       matchAll: true,
-       template: Template.autocompleteStrategy
-     },
-     {
-       token: '!',
-       collection: Events,
-       field: "title",
-       options: '',
-       matchAll: true,
-       filter: { type: "autocomplete" },
-       template: Template.autocompleteEvent
-     }
-   ]
-  }
-};
+       {
+         token: '',
+         collection: Strategies,
+         field: "name",
+         matchAll: true,
+         template: Template.autocompleteStrategy
+       },
+       {
+         token: '!',
+         collection: Events,
+         field: "title",
+         options: '',
+         matchAll: true,
+         filter: { type: "autocomplete" },
+         template: Template.autocompleteEvent
+       }
+    ]
+    }
+  };
 
 }
