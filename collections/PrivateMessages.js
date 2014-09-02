7b47a150317513481a6b6448f68660ce55935499
Andrei Cristof <andreimcristof@gmail.com>
2014-09-03 00:44:33 +0200

    some layout fixes

---
+++ collections/PrivateMessages.js
@@ -0,0 +1,15 @@
+PrivateMessages = new Meteor.Collection("privatemessages");
+
+/*
+
+	fields:
+
+	_id
+	userId
+	contenet
+	status: "favorited", ... 
+	...TODO: 
+		figure out if other fields are needed 
+		option to block user
+		option to favorite user
+*/
